ae1000c89514194df4438cc1419d1335528918ae
tomazini marcos <marcos.tomazini@gmail.com>
2017-11-30 14:30:40 -0200

    ultimas melhorias

--- Compare2Files/Program.cs
+++ Compare2Files/Program.cs
@@ -1,15 +1,151 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Compare2Files
 {
+    class teste
+    {
+        public teste()
+        {
+
+        }
+        public teste(string t)
+        {
+            Descricao = t;
+        }
+        public string Descricao { get; set; }
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
+            var especialidade = new teste();
+            var especialidade2 = new teste();
+
+            var especialidades = new List<teste>();
+            var t = new teste("teste"); 
+            especialidades.Add(new teste("RADIOLOGIA ODONTOLOGIA"));
+
+            especialidades.Add(t);
+
+            var listaEspecialidadeURA = new Dictionary<int, string>();
+            listaEspecialidadeURA.Add(991, "CLINICA GERAL;CLINICA MEDICA");
+            listaEspecialidadeURA.Add(994, "ENDODONTIA");
+            listaEspecialidadeURA.Add(1000, "CIRURGIA");
+            listaEspecialidadeURA.Add(998, "PERIODONTIA");
+            listaEspecialidadeURA.Add(995, "ODONTOPEDIATRIA");
+            listaEspecialidadeURA.Add(993, "RADIOLOGIA");
+            listaEspecialidadeURA.Add(711, "GINECOLOGIA");
+            listaEspecialidadeURA.Add(721, "OBSTETRICIA");
+            listaEspecialidadeURA.Add(1263, "ORTOPEDIA");
+            listaEspecialidadeURA.Add(136, "OFTALMOLOGIA");
+            listaEspecialidadeURA.Add(117, "DERMATOLOGIA");
+            listaEspecialidadeURA.Add(140, "PEDIATRIA");
+            listaEspecialidadeURA.Add(105, "CARDIOLOGIA");
+            listaEspecialidadeURA.Add(118, "ENDOCRINOLOGIA");
+
+            string descricaoEspecialidade = "TESTE;RADIOasdLOGIA";
+            var arrayEspc = descricaoEspecialidade.Split(';');
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            if (arrayEspc.Length > 1)
+            {
+                especialidade = especialidades
+                    .FirstOrDefault(e =>
+                    e.Descricao.ToUpper().Contains(arrayEspc[0]));
+                if (especialidade == null)
+                {
+                    especialidade = especialidades
+                        .FirstOrDefault(e =>
+                        e.Descricao.ToUpper().Contains(arrayEspc[1]));
+                }
+            }
+            else
+            {
+                especialidade = especialidades
+                    .FirstOrDefault(e =>
+                    e.Descricao.ToUpper().Contains(arrayEspc[0]));
+            }
+
+            var tempo1 = sw.Elapsed;      
+            sw.Stop();
+
+            sw = Stopwatch.StartNew();            
+
+            especialidade2 = especialidades
+                   .FirstOrDefault(e => arrayEspc.Any(e.Descricao.ToUpper().Contains));
+
+            var tempo2 = sw.Elapsed;
+            sw.Stop();
+
+            var asd = "RADIOLOGIA ODONTOLOGIA".ToUpper().Contains(arrayEspc[1]);
+
+            var sad = arrayEspc.Any("RADIOLOGIA ODONTOLOGIA".ToUpper().Contains);
+
+            var dsa = Array.IndexOf(arrayEspc, "RADIOLOGIA ODONTOLOGIA".ToUpper());
+
+            var sdaasdespecialidade = especialidades
+                   .FirstOrDefault(e => arrayEspc.Any(e.Descricao.ToUpper().Contains));
+
+            var asdespecialidade = especialidades
+                   .Where(e => arrayEspc.Any(e.Descricao.ToUpper().Contains)).FirstOrDefault();
+
+            especialidade = especialidades
+                   .FirstOrDefault(e => arrayEspc.Contains(e.Descricao.ToUpper()));
+
+
+
+            var asdsa = abreviarNome("RECOGNITION COMPANHIA BRASILEIRA DE AUTOMACAO BANCARIA");
+            String directory = @"D:\Projetos\CompareTwoFiles\Files\";
+            String[] linesA = File.ReadAllLines(Path.Combine(directory, "DEPOIS COM PERWDOCDM1_50.20170913"));
+            String[] linesB = File.ReadAllLines(Path.Combine(directory, "DEPOIS SEM PERWDOCDM1_50.20170918"));
+
+            IEnumerable<String> onlyB = linesB.Except(linesA);
+
+            File.WriteAllLines(Path.Combine(directory, "Result.txt"), onlyB);
+        }
+
+        private static string abreviarNome(string nome)
+        {
+            var exceccoes = new String[] { "ME", "LTDA", "SA", "MEI", "EPP", "EIRELI", "Individual" };
+
+            if (string.IsNullOrEmpty(nome))
+            {
+                return nome;
+            }
+            else
+            {
+                var tokens = nome.Trim().Split(' ');
+                var tokensNomes = new List<string>();
+                foreach (var t in tokens)
+                {
+                    if (!string.IsNullOrWhiteSpace(t))
+                    {
+                        tokensNomes.Add(t);
+                    }
+                }
+                if (tokensNomes.Count > 2)
+                {
+                    var nomeAbreviado = tokens[0] + " ";
+                    for (int i = 1; i < tokensNomes.Count - 1; i++)
+                    {
+                        if (!exceccoes.Contains(tokensNomes[i]))
+                            nomeAbreviado += tokensNomes[i][0] + ". ";
+                        else
+                            nomeAbreviado += tokensNomes[i] + " ";
+                    }
+                    return nomeAbreviado += tokensNomes.Last();
+                }
+                return nome;
+            }
         }
     }
 }
---
+++ Compare2FilesForms/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compare2FilesForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                label1.Text = (openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                label2.Text = (openFileDialog1.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var palavraIgnorada = string.Empty;
+            var letra = "X";
+
+            if (label1.Text == "Arquivo 1" || label2.Text == "Arquivo 2")
+            {
+                MessageBox.Show("Selecione os arquivos!!!", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] linesA = File.ReadAllLines(label1.Text);
+            string[] linesB = File.ReadAllLines(label2.Text);
+
+
+            var ignorarColunas = new List<Tuple<int, int>>();
+
+            //ignorarColunas.Add(new Tuple<int, int>(71, 10));
+            ignorarColunas.Add(new Tuple<int, int>(477, 10)); // Data da ocorrência do sinistro.
+            ignorarColunas.Add(new Tuple<int, int>(804, 15)); // nosso numero
+
+            palavraIgnorada = string.Empty;
+            foreach (var item in ignorarColunas.OrderBy(x => x.Item1))
+            {
+                palavraIgnorada = letra;
+                for (int i = 1; i < (item.Item2); i++)
+                {
+                    palavraIgnorada += letra;
+                }
+
+                linesA = linesA.Select(x => x.Remove(item.Item1, item.Item2).Insert(item.Item1, palavraIgnorada)).ToArray();
+                linesB = linesB.Select(x => x.Remove(item.Item1, item.Item2).Insert(item.Item1, palavraIgnorada)).ToArray();
+            }
+            
+
+            IList<string> onlyA = linesA.Except(linesB).ToList();
+            IList<string> onlyB = linesB.Except(linesA).ToList();
+
+            var total = onlyA.Union(onlyB).Distinct();
+
+            if (!total.Any())
+            {
+                MessageBox.Show("Arquivo idêntico!", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(string.Format("Foi encontrado diferenças nos arquivos.\n\nTotal de diferenças: {0} linhas.\n\nDeseja salvar as diferenças ?", onlyA.Distinct().Count()), 
+                "Confirmação", 
+                MessageBoxButtons.YesNo, 
+                MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {            
+                SaveFileDialog savefile = new SaveFileDialog();
+                // set a default file name
+                savefile.FileName = string.Format("Comparacao{0}.txt", DateTime.Now.ToString("yyyyMMdd"));
+                // set filters - this can be done in properties as well
+                savefile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    var directory = Path.GetDirectoryName(savefile.FileName);
+                    var asad = Path.GetFileNameWithoutExtension(savefile.FileName);
+
+                    //File.WriteAllLines(Path.Combine(directory, string.Format("Comparacao{0}.txt", DateTime.Now.ToString("yyyyMMdd"))), total);
+                    File.WriteAllLines(Path.Combine(directory, string.Format("Comparacao{0}_1.txt", DateTime.Now.ToString("yyyyMMdd"))), onlyA);
+                    File.WriteAllLines(Path.Combine(directory, string.Format("Comparacao{0}_2.txt", DateTime.Now.ToString("yyyyMMdd"))), onlyB);
+                    MessageBox.Show("Salvo com sucesso!", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            
+        }
+    }
+}
